e00afe1bb7a7999d9547c15026bdde101209c603
DanielShmuel <daniel@vayusense.com>
2020-01-23 11:31:11 +0200

    add config server

---
+++ src/test/java/com/vayusense/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vayusense.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
